674ea79325d205a6ace8ac265b5b8bd61933cad4
shanmukhkalasamudram <shanmukhkalasamudram@gmail.com>
2020-11-13 00:03:45 +0530

    Sentance Chnage

--- pub/js/app.js
+++ pub/js/app.js
@@ -1,4 +1,4 @@
-console.log('Client side javascript file is loaded!')
+console.log('Client side javascript file is loadd!')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
